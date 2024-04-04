f0bf01fa46395feb0a9ba26983920b64ac08d336
Luca Casati <luk00casati@gmail.com>
2024-04-04 18:59:32 +0200

    comment unused change key

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -30,16 +30,15 @@ static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 static const int momentaryalttags =
     0; /* 1 means alttags will show only when key is held down*/
 
-static const Rule rules[] =
-    {
-        /* xprop(1):
-         *	WM_CLASS(STRING) = instance, class
-         *	WM_NAME(STRING) = title
-         */
-        /* class      instance    title       tags mask   allowkill isfloating
-           monitor */
-        {"Brave", NULL, NULL, 1 << 8, 0, 0, -1},
-        //   { "Github-desktop", NULL, NULL,         1 << 2,       1, 0, -1 }
+static const Rule rules[] = {
+    /* xprop(1):
+     *	WM_CLASS(STRING) = instance, class
+     *	WM_NAME(STRING) = title
+     */
+    /* class      instance    title       tags mask   allowkill isfloating
+       monitor */
+    {"Brave", NULL, NULL, 1 << 8, 0, 0, -1},
+    //   { "Github-desktop", NULL, NULL,         1 << 2,       1, 0, -1 }
 };
 
 /* layout(s) */
@@ -54,7 +53,7 @@ static const Layout layouts[] = {
     /* symbol     arrange function */
     {"T", tile}, /* first entry is default */
     {"F", NULL}, /* no layout function means floating behavior */
-    //	{ "[M]",      monocle },
+                 //	{ "[M]",      monocle },
 };
 
 /* key definitions */
@@ -91,7 +90,7 @@ static const char *dmenucmd[] = {"dmenu_run_history",
                                  "-sf",
                                  col_gray4,
                                  NULL};
-static const char *termcmd[] = {"st", NULL};
+// static const char *termcmd[] = {"st", NULL};
 static const char *flameguicmd[] = {"flameshot", "gui", NULL};
 static const char *flamescreencmd[] = {"flameshot", "screen", NULL};
 static const char *tabstcmd[] = {"tabbed", "-r", "2", "st", "-w", "''", NULL};
@@ -119,21 +118,29 @@ static const Key keys[] = {
     //	{ MODKEY,                       XK_period,    view,           {0} },
     {MODKEY, XK_q, killclient, {0}},
     //	{ MODKEY,                       XK_t,      setlayout,      {.v =
-    //&layouts[0]} }, 	{ MODKEY,                       XK_f,      setlayout, {.v
-    //= &layouts[1]} }, 	{ MODKEY,                       XK_m,      setlayout,
+    //&layouts[0]} }, 	{ MODKEY,                       XK_f,      setlayout,
+    //{.v = &layouts[1]} }, 	{ MODKEY,                       XK_m, setlayout,
     //{.v = &layouts[2]} },
     {MODKEY, XK_space, setlayout, {0}},
     {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
     {MODKEY, XK_f, togglefullscr, {0}},
     //	{ MODKEY,                       XK_0,      view,           {.ui = ~0 }
-    //}, 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 }
-    //}, 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 }
-    //}, 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 }
-    //}, 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 }
-    //}, 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 }
-    //}, 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 }
-    //}, 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 }
-    //}, 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  }
+    //}, 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui
+    //= ~0 }
+    //}, 	{ MODKEY,                       XK_comma,  focusmon,       {.i =
+    //-1 }
+    //}, 	{ MODKEY,                       XK_period, focusmon,       {.i =
+    //+1 }
+    //}, 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i =
+    //-1 }
+    //}, 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i =
+    //+1 }
+    //}, 	{ MODKEY,                       XK_minus,  setgaps,        {.i =
+    //-1 }
+    //}, 	{ MODKEY,                       XK_equal,  setgaps,        {.i =
+    //+1 }
+    //}, 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i =
+    //0  }
     //},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
@@ -143,22 +150,21 @@ static const Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
  * ClkClientWin, or ClkRootWin */
-static const Button buttons[] =
-    {
-        /* click                event mask      button          function
-           argument */
-        //	{ ClkLtSymbol,          0,              Button1, setlayout, {0}
-        //}, 	{ ClkLtSymbol,          0,              Button3,        setlayout,
-        //{.v = &layouts[2]} }, 	{ ClkWinTitle,          0,              Button2,
-        //zoom,           {0} }, 	{ ClkStatusText,        0, Button2, spawn, {.v
-        //= termcmd } },
-        {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-        //	{ ClkClientWin,         MODKEY,         Button2, togglefloating,
-        //{0} },
-        {ClkClientWin, MODKEY | ShiftMask, Button1, resizemouse, {0}},
-        //	{ ClkTagBar,            0,              Button1,        view,
-        //{0} }, 	{ ClkTagBar,            0,              Button3, toggleview,
-        //{0} }, 	{ ClkTagBar,            MODKEY,         Button1,        tag,
-        //{0} }, 	{ ClkTagBar,            MODKEY,         Button3, toggletag, {0}
-        //},
+static const Button buttons[] = {
+    /* click                event mask      button          function
+       argument */
+    //	{ ClkLtSymbol,          0,              Button1, setlayout, {0}
+    //}, 	{ ClkLtSymbol,          0,              Button3, setlayout,
+    //{.v = &layouts[2]} }, 	{ ClkWinTitle,          0,              Button2,
+    // zoom,           {0} }, 	{ ClkStatusText,        0, Button2, spawn, {.v
+    //= termcmd } },
+    {ClkClientWin, MODKEY, Button1, movemouse, {0}},
+    //	{ ClkClientWin,         MODKEY,         Button2, togglefloating,
+    //{0} },
+    {ClkClientWin, MODKEY | ShiftMask, Button1, resizemouse, {0}},
+    //	{ ClkTagBar,            0,              Button1,        view,
+    //{0} }, 	{ ClkTagBar,            0,              Button3, toggleview,
+    //{0} }, 	{ ClkTagBar,            MODKEY,         Button1,        tag,
+    //{0} }, 	{ ClkTagBar,            MODKEY,         Button3, toggletag, {0}
+    //},
 };
